2e32583077316624102c24a1fd248895ac496984
darko <ki14.skakun@gmail.com>
2024-01-09 14:40:30 +0200

    added message db model

--- demochat/asgi.py
+++ demochat/asgi.py
@@ -1,16 +1,21 @@
-"""
-ASGI config for demochat project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
+from chat import routing
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demochat.settings')
 
-application = get_asgi_application()
+
+django_asgi_application = get_asgi_application()
+
+application = ProtocolTypeRouter(
+    {
+        'http': django_asgi_application,
+        'websocket': AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
+        )
+    }
+)
